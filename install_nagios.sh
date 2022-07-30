c0b03591ebf3bf599415a25121657bc1af6c5344
abdullah01455 <abdullah01455@hotmail.com>
2022-07-30 17:18:42 +0300

    add script

--- install_nagios.sh
+++ install_nagios.sh
@@ -1,29 +1,16 @@
-sudo su
-yum install httpd php -y
-yum install gcc glibc glibc-common -y
-yum install gd gd-devel -y
+#!/bin/bash
+sudo amazon-linux-extras install epel -y
+sudo yum install nagios nrpe nagios-plugins-all
+sudo chkconfig -–level 3 nagios on
+sudo service httpd start
+sudo chkconfig httpd on
+sudo yum install php
+#edit
 
-adduser -m nagios
-passwd nagios
+sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg
 
-groupadd nagioscmd
-usermod -a -G nagioscmd nagios
-usermod -a -G nagioscmd apache
-
-mkdir ~/downloads
-cd ~/downloads
-
-wget https://webwerks.dl.sourceforge.net/project/nagios/nagios-4.x/nagios-4.0.8/nagios-4.0.8.tar.gz
-wget http://nagios-plugins.org/download/nagios-plugins-2.0.3.tar.gz
-
-tar zxvf nagios-4.0.8.tar.gz
-cd nagios-4.0.8
-
-./configure --with-command-group=nagioscmd
-make all
+sudo service nagios start
 
 
-make install
-make install-init
-make install-config
-make install-commandmode
+
+
